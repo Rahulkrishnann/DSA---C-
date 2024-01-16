059276d4182728088eccfb6a55fbddecc55b9081
Rahulkrishnann <rahultamilselvanba.19ece@kongu.edu>
2024-01-17 05:05:16 +0530

    updated

--- Array/BuildArrayFromPermutation_1920.cpp
+++ Array/BuildArrayFromPermutation_1920.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
         vector<int> ans;
-        vector<int> nums2;
+        vector<int> num2;
         for(int i=0;i<nums.size();i++){
-           nums2.push_back(nums[nums[i]]);
-           ans.push_back(nums2[i]);
+           num2.push_back(nums[nums[i]]);
+           ans.push_back(num2[i]);
         }
         return ans;
     }
